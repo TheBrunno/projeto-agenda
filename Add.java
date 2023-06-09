0c038e08c2e3aef776f9247a8258b526316ce667
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-06-08 22:26:20 -0300

    fix: add screen layout

--- Add.java
+++ Add.java
@@ -15,14 +15,13 @@ public class Add extends Gui{
         JButton tasks = new Button("Tarefa", 200, 100).getButton();
         JButton events = new Button("Eventos", 200, 100).getButton();
         JButton notes = new Button("Notas", 200, 100).getButton();
-        JButton back = new Button("images/back.png", 40, 40, 100, 100, "").getButton();
+        JButton back = new Button("images/back.png", 50, 50, 70, 70, "").getButton();
         JButton trash = new Button("images/trash.png", 40, 50, 70, 70, "").getButton();
         JButton settings = new Button("images/config.png", 50, 50, 70, 70, "").getButton();
 
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JPanel choicesPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         JLabel label = new JLabel("Escolha uma ação:");
@@ -37,15 +36,12 @@ public class Add extends Gui{
         panel.add(labelPanel, BorderLayout.NORTH);
         panel.add(choicesPanel, BorderLayout.SOUTH);
 
-        topPanel.add(back);
-
+        bottomPanel.add(back);
         bottomPanel.add(trash);
         bottomPanel.add(settings);
 
         panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        topPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        this.jframe.add(topPanel, BorderLayout.NORTH);
         this.jframe.add(panel, BorderLayout.CENTER);
         this.jframe.add(bottomPanel, BorderLayout.SOUTH);
 
